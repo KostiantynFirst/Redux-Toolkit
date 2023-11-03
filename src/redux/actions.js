52214262d80e0b5ac749a07f8d3cef093d5a046e
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-11-03 21:52:52 +0200

    Update actions.js

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,5 +1,4 @@
-import { createAction } from '@reduxjs/toolkit';
-import { nanoid } from 'nanoid';
+import { createAction, nanoid } from '@reduxjs/toolkit';
 
 export const addTask = createAction('tasks/addTask', (text) => {
     return {
